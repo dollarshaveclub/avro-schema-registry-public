431ccd5fbea6bbb099fdbcf7ca20c1c755586a15
Brian McKelvey <theturtle32@gmail.com>
2020-03-27 21:37:55 -0700

    Instrument datadog apm for grape

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,6 +2,7 @@ if ENV.key?('DATADOG_APM_SERVICE_NAME')
   if defined?(Datadog)
     Datadog.configure do |c|
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
+      c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}-grape"
     end
   else
     puts 'DATADOG_APM_SERVICE_NAME specified, but ddtrace gem not loaded!'
