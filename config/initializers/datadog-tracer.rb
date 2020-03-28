2548371864b759d3eeb5ce906208420b59bf31ee
Brian McKelvey <theturtle32@gmail.com>
2020-03-27 22:37:37 -0700

    Trying using the same service name for grape and rails.

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -3,7 +3,7 @@ if ENV.key?('DATADOG_APM_SERVICE_NAME')
     Datadog.configure do |c|
       c.tracer env: Rails.env, enabled: true
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
-      c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}-grape"
+      c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}"
     end
   else
     puts 'DATADOG_APM_SERVICE_NAME specified, but ddtrace gem not loaded!'
