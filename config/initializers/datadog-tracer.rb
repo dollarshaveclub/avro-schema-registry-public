8116e7463687b2255df98d922b55469c95530c89
Brian McKelvey <theturtle32@gmail.com>
2020-03-27 21:56:42 -0700

    Reverting env change.

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,7 +1,6 @@
 if ENV.key?('DATADOG_APM_SERVICE_NAME')
   if defined?(Datadog)
     Datadog.configure do |c|
-      c.env = Rails.env
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
       c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}-grape"
     end
