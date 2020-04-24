639c4a409fc483752f081f3464a79bea95bcd10e
Brian McKelvey <brian.mckelvey@dollarshaveclub.com>
2020-04-24 14:01:02 -0700

    Fix datadog config format... again...

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,9 +2,9 @@ if ENV.key?('DATADOG_APM_SERVICE_NAME')
   if defined?(Datadog)
     Datadog.configure do |c|
       c.tracer.enabled = true
-      c.tracer.env = Rails.env
       c.tracer.hostname = ENV['DATADOG_TRACER_HOSTNAME'] || 'localhost'
       c.tracer.port = ENV['DATADOG_TRACER_PORT'] || 8126
+      c.env = Rails.env
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
       c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}"
     end
