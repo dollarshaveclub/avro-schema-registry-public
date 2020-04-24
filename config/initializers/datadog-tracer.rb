e87d8f60789619e145c0834931e5a343c86a993d
Brian McKelvey <theturtle32@gmail.com>
2020-04-24 13:54:27 -0700

    Updating ddtrace gem and configuration format.

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,9 +1,10 @@
 if ENV.key?('DATADOG_APM_SERVICE_NAME')
   if defined?(Datadog)
     Datadog.configure do |c|
-      c.tracer env: Rails.env, enabled: true,
-               hostname: ENV['DATADOG_TRACER_HOSTNAME'] || 'localhost',
-               port: ENV['DATADOG_TRACER_PORT'] || 8126
+      c.tracer.enabled = true
+      c.tracer.env = Rails.env
+      c.tracer.hostname = ENV['DATADOG_TRACER_HOSTNAME'] || 'localhost'
+      c.tracer.port = ENV['DATADOG_TRACER_PORT'] || 8126
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
       c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}"
     end
@@ -11,3 +12,4 @@ if ENV.key?('DATADOG_APM_SERVICE_NAME')
     puts 'DATADOG_APM_SERVICE_NAME specified, but ddtrace gem not loaded!'
   end
 end
+
