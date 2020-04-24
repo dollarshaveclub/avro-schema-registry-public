490f7a1bdbb423d61f29fa9f37cffb5907282be2
Brian McKelvey <brian.mckelvey@dollarshaveclub.com>
2020-04-24 12:32:19 -0700

    Add ability to configure Datadog Agent host/port
    @esterlinej

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,6 +2,8 @@ if ENV.key?('DATADOG_APM_SERVICE_NAME')
   if defined?(Datadog)
     Datadog.configure do |c|
       c.tracer env: Rails.env, enabled: true
+      c.tracer.hostname = ENV['DATADOG_TRACER_HOSTNAME'] || 'localhost'
+      c.tracer.port = ENV['DATADOG_TRACER_PORT'] || 8126
       c.use :rails, service_name: ENV['DATADOG_APM_SERVICE_NAME']
       c.use :grape, service_name: "#{ENV['DATADOG_APM_SERVICE_NAME']}"
     end
